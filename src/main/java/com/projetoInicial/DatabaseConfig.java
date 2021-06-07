87493ae94cdb035d77c8fb00b4f9b2fc7b60d4fb
pedro-rd-dev <pedro.rd.dev@gmail.com>
2021-06-07 04:22:53 -0300

    Usando o banco de dados do postgress

---
+++ src/main/java/com/projetoInicial/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package com.projetoInicial;
+
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+public class DatabaseConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
